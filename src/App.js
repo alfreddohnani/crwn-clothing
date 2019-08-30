8d251aafd17bd55e952034295347ead1d87fc96a
Alfred <alfred.doh-nani@meltwater.org>
2019-08-30 06:15:42 +0000

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SignInAndSignUpPage from  "./pages/sign-in-and-sign-up/sign-in-and-sign-u
 import { Switch, Route} from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth,    createUserProfileDocument } from "./firebase/firebase.utils";
 
 
 
@@ -21,9 +21,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-  this.unsubscribeFromAuth =  auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
+  this.unsubscribeFromAuth =  auth.onAuthStateChanged(userAuth => {
+    if(userAuth) {
+      const userRef =  createUserProfileDocument(userAuth); 
 
+      userRef.onSnapshot(snapShot => {
+        this.setState({
+          currentUser: {
+            id: snapShot.id,
+            ...snapShot.data()
+          }
+        })
+      });
+    } else {
+      this.setState({ currentUser: userAuth });
+    }
     })
   }
 
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -3,10 +3,12 @@ import "./sign-in-and-sign-up.styles.scss";
 import FormInput from "../../components/form-input/form-input.component";
 import SignIn from "../../components/sign-in/sign-in.component";
 import CustomButton from "../../components/custom-button/custom-button.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const SignInAndSignUpPage = () => (
     <div className="sign-in-and-sign-up">
         <SignIn />
+        <SignUp />
     </div>
 );
 
